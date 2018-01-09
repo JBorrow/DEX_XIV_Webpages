a256189b807b2686b91792a14df08620c732d0e7
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-01-09 13:59:20 +0000

    Added ability to include links to talks.

--- compile.py
+++ compile.py
@@ -6,6 +6,8 @@ the structure as an object to our template.
 import jinja2
 import yaml
 
+import numpy as np
+
 
 def get_from_filesystem(templates, data_fn, shared_data=None):
     """ Takes a list of template names and returns a zipped list of template
@@ -14,8 +16,10 @@ def get_from_filesystem(templates, data_fn, shared_data=None):
 
     template_loader = jinja2.FileSystemLoader(searchpath="./src")
     template_env = jinja2.Environment(loader=template_loader)
+    template_env.filters["talk_grab"] = talk_grab
 
     templates = list(map(template_env.get_template, templates))
+
 
     shared = get_data(shared_data)
     data = get_data(data_fn)
@@ -26,6 +30,23 @@ def get_from_filesystem(templates, data_fn, shared_data=None):
     out_data = [dict(d, **s) for d, s in zip(data, shared)]
     
     return zip(templates, out_data)
+
+
+def talk_grab(name):
+    """
+    Grab a talk
+    """
+
+    data = np.loadtxt("src/talks.csv", usecols=(0, 1), dtype=str, delimiter=",")
+
+    where = np.where(data[:, 0] == name)
+
+    filename = data[where, 1]
+
+    try:
+        return filename[0][0]
+    except:
+        return ""
 
 
 def get_data(data_fn):
